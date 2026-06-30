bd7eda7d0b5165e1786079eaa1b7fcde3615245f
anichokwenda <anichokwenda@gmail.com>
2026-06-30 13:31:57 +0200

    commit

--- server.js
+++ server.js
@@ -13,16 +13,33 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/**
-  * Configure Express middleware
-  */
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+/// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
 /**
   * Routes
   */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
